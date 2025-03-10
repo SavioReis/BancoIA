2ec312c0548b73f0d5b9ed4c79fa7b8e4e4e8387
SavioReis <72362096+SavioReis@users.noreply.github.com>
2025-03-10 12:13:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,154 @@
+const ferramentas = {
+    imagem: [
+        { nome: "KREA", descricao: "KREA", imagem: "img/krea.png", link: "https://openai.com/dall-e" },
+        { nome: "Ideogram", descricao: "Ideogram", imagem: "img/ideogram.png", link: "https://ideogram.ai/login" },
+        { nome: "Dall-E", descricao: "Dall-E", imagem: "img/Dall.png", link: "https://openai.com/index/dall-e-2/" },
+        { nome: "Midjourney", descricao: "Midjourney", imagem: "img/Midjourney.png", link: "https://www.midjourney.com/explore?tab=top" },
+        { nome: "Blue Willow", descricao: "Blue Willow", imagem: "img/Blue.png", link: "https://www.bluewillow.ai/" },
+        { nome: "Gemini Vision", descricao: "Gemini Vision", imagem: "img/Vision.png", link: "https://gemini.google.com/app" },
+        { nome: "Seaart.AI", descricao: "Seaart.AI", imagem: "img/Seaart.png", link: "https://www.seaart.ai/pt?https%3A%2F%2Fwww.seaart.ai%2Fhome%3F&ad=WWGGC1SEARCH&gad_source=1&gclid=CjwKCAjwk8e1BhALEiwAc8MHiC_3B1jHVRJy7JPXZ0v285c4uJwofTjKVuz49HE2Tn4Cj2J8vw2ydBoCYTkQAvD_BwE" },
+        { nome: "Leonardo.ai", descricao: "Leonardo.ai", imagem: "img/Leonardo.png", link: "https://leonardo.ai/" },
+        { nome: "PRomeai", descricao: "PRomeai", imagem: "img/PRomeai.png", link: "https://www.promeai.pro/pt" },
+        { nome: "Huggin Face", descricao: "Huggin Face", imagem: "img/Huggin.png", link: "https://huggingface.co/spaces/artificialguybr/Pixart-Sigma" },
+        { nome: "Meta AI", descricao: "Meta AI", imagem: "img/Meta.png", link: "https://www.meta.ai/" },
+        { nome: "Tensor AI", descricao: "Tensor AI", imagem: "img/Tensor.png", link: "https://tensor.art/posts" },
+        { nome: "Lexica", descricao: "Lexica", imagem: "img/Lexica.png", link: "https://lexica.art/" },
+        { nome: "Maze Guru", descricao: "Maze Guru", imagem: "img/Maze.png", link: "https://maze.guru/" },
+        { nome: "playground ai", descricao: "playground ai", imagem: "img/playground.png", link: "https://playground.com/" },
+        { nome: "Mega space", descricao: "Mega space", imagem: "img/Mega.png", link: "https://www.mage.space/" }
+    ],
+    video: [
+        { nome: "heiper", descricao: "heiper", imagem: "img/heiper.png", link: "https://haiper.ai/home" },
+        { nome: "KREA", descricao: "KREA", imagem: "img/krea.png", link: "https://www.krea.ai/apps/image/flux" },
+        { nome: "Kling", descricao: "Kling", imagem: "img/Kling.png", link: "https://klingai.com/" },
+        { nome: "PIKA - ORIGINAL", descricao: "PIKA - ORIGINAL", imagem: "img/PIKA.png", link: "https://pika.art/login" },
+        { nome: "Pika Labs", descricao: "Pika Labs", imagem: "img/Labs.png", link: "https://pikalabsai.org/text-to-video/" },
+        { nome: "Meta AI", descricao: "Meta AI", imagem: "img/Meta.png", link: "https://www.meta.ai/" },
+        { nome: "Invideo AI", descricao: "Invideo AI", imagem: "img/Invideo.png", link: "https://invideo.io/make/ai-web/?utm_source=google&utm_medium=cpc&utm_campaign=Global_Search_Brand_Exact_EN&adset_name=Invideo_AI&keyword=invideo%20.ai&network=g&device=c&utm_term=invideo%20.ai&utm_content=Invideo_AI&matchtype=e&placement=g&campaign_id=18035330774&adset_id=152533182894&ad_id=674819400174&gad_source=1&gclid=CjwKCAjwk8e1BhALEiwAc8MHiGg4mN2Vsezh-DgUVhnRhWR8x908CnGRv4n6oXOILbh_BjVA7rUCphoCh5MQAvD_BwE" },
+        { nome: "Pixverse.ai", descricao: "Pixverse.ai", imagem: "img/Pixverse.png", link: "https://app.pixverse.ai/login?utm_source=Google&utm_medium=SearchAds&utm_campaign=WW-RE-Brand&utm_id=v240716&utm_term=PixVerse-AI&gad_source=1&gclid=CjwKCAjwk8e1BhALEiwAc8MHiIDhEVbuOVvyw2lGDNx723muhxOUa9UxXeb-HzdjeUg0zfcD11UHthoCljEQAvD_BwE" },
+        { nome: "VIVA", descricao: "VIVA", imagem: "img/VIVA.png", link: "https://vivago.ai/home" },
+        { nome: "Runway ai", descricao: "Runway ai", imagem: "img/Runway.png", link: "https://runwayml.com/" },
+        { nome: "Genmo.ai", descricao: "Genmo.ai", imagem: "img/Genmo.png", link: "https://www.genmo.ai/" }
+    ],
+    texto: [
+        { nome: "ChatGPT", descricao: "ChatGPT", imagem: "img/ChatGPT.png", link: "https://chatgpt.com/" },
+        { nome: "Gemini", descricao: "Gemini", imagem: "img/Gemini.png", link: "https://gemini.google.com/app" },
+        { nome: "Claude", descricao: "Claude", imagem: "img/Claude.png", link: "https://claude.ai/new" },
+        { nome: "Writesonic", descricao: "Writesonic", imagem: "img/Writesonic.png", link: "https://writesonic.com/chat" },
+        { nome: "you", descricao: "you", imagem: "img/you.png", link: "https://you.com/" },
+        { nome: "Copilot", descricao: "Copilot", imagem: "img/Copilot.png", link: "https://copilot.microsoft.com/chats/caVWmK6H23pPLMxzBi98b" },
+        { nome: "PI.AI", descricao: "PI.AI", imagem: "img/PI.png", link: "https://pi.ai/onboarding" },
+        { nome: "Perplexity", descricao: "Perplexity", imagem: "img/Perplexity.png", link: "https://www.perplexity.ai/" },
+        { nome: "Jasper.AI", descricao: "Jasper.AI", imagem: "img/Jasper.png", link: "" },
+        { nome: "Copy.AI", descricao: "Copy.AI", imagem: "img/Copy.png", link: "https://www.copy.ai/" },
+        { nome: "character.ai", descricao: "character.ai", imagem: "img/character.png", link: "https://character.ai/" },
+        { nome: "POE", descricao: "POE", imagem: "img/POE.png", link: "https://poe.com/login" }
+    ],
+    animar: [
+        { nome: "Hailou", descricao: "Hailou", imagem: "img/Hailou.png", link: "https://hailuoai.video/" },
+        { nome: "heiper", descricao: "heiper", imagem: "img/heiper.png", link: "https://haiper.ai/home" },
+        { nome: "Immersity", descricao: "Immersity", imagem: "img/Immersity.png", link: "https://www.immersity.ai/" },
+        { nome: "Runway ai", descricao: "Runway ai", imagem: "img/Runway.png", link: "https://runwayml.com/" },
+        { nome: "META demolab", descricao: "META demolab", imagem: "img/META.png", link: "https://sketch.metademolab.com/" },
+        { nome: "Replicate", descricao: "Replicate", imagem: "img/Replicate.png", link: "https://replicate.com/stability-ai/stable-video-diffusion" },
+        { nome: "VIVA", descricao: "VIVA", imagem: "img/VIVA.png", link: "https://vivago.ai/home" },
+        { nome: "Kling", descricao: "Kling", imagem: "img/Kling.png", link: "https://klingai.com/" },
+        { nome: "Leonard", descricao: "Leonard", imagem: "img/Leonardo.png", link: "https://leonardo.ai/" },
+        { nome: "D-ID", descricao: "D-ID", imagem: "img/DID.png", link: "https://www.d-id.com/" }
+    ],
+    avatar: [
+        { nome: "Heygen", descricao: "Heygen", imagem: "img/Heygen.png", link: "https://www.heygen.com/" },
+        { nome: "D-ID", descricao: "D-ID", imagem: "img/DID.png", link: "https://www.d-id.com/" },
+        { nome: "Vidnoz", descricao: "Vidnoz", imagem: "img/Vidnoz.png", link: "https://www.vidnoz.com/?a_aid=655225dd0d18e" },
+        { nome: "YEPIC.AI", descricao: "YEPIC.AI", imagem: "img/YEPIC.png", link: "https://www.yepic.ai/?via=cristian" },
+        { nome: "TOKKINGHEADS", descricao: "TOKKINGHEADS", imagem: "img/TOKKINGHEADS.png", link: "https://app.tokkingheads.com/homepage" }
+    ],
+    narracao: [
+        { nome: "Eleven Labs", descricao: "Eleven Labs", imagem: "img/Eleven.png", link: "https://elevenlabs.io/" },
+        { nome: "Natural Reader", descricao: "Natural Reader", imagem: "img/Natural.png", link: "https://www.naturalreaders.com/" },
+        { nome: "Ttsmaker", descricao: "Ttsmaker", imagem: "img/Ttsmaker.png", link: "https://ttsmaker.com/" },
+        { nome: "Voicefy", descricao: "Voicefy", imagem: "img/Voicefy.png", link: "https://www.voicefy.com.br/?gad_source=1&gclid=Cj0KCQjwtsy1BhD7ARIsAHOi4xa9ZUS1qzpNHXGo0kMcWNMU_lpU8qybs8TLyaY1PHliS_t_19MycFQaAkn9EALw_wcB" },
+        { nome: "Capcut", descricao: "Capcut", imagem: "img/Capcut.png", link: "https://www.capcut.com/pt-br/" }
+    ],
+    tlegenda: [
+        { nome: "GhostCut", descricao: "GhostCut", imagem: "img/GhostCut.png", link: "https://jollytoday.com/" }
+    ],
+    trvideos: [
+        { nome: "Riversider", descricao: "Riversider", imagem: "img/Riversider.png", link: "https://riverside.fm/?utm_term=riverside&utm_campaign=google_search_ww_desktop_brand_phrase&utm_source=google&utm_medium=cpc&hsa_acc=5612739488&hsa_cam=17284051134&hsa_grp=138500287164&hsa_ad=598676487782&hsa_src=g&hsa_tgt=kwd-94177033&hsa_kw=riverside&hsa_mt=p&hsa_net=adwords&hsa_ver=3&gad_source=1&gclid=Cj0KCQjwq_G1BhCSARIsACc7Nxp5W-KM83aT7PI4arvljKg05stW-PpfT_YhfW0JrTkWmRX5Zpz7s1saAkgrEALw_wcB" },
+        { nome: "ASSEMBLY AI", descricao: "ASSEMBLY AI", imagem: "img/ASSEMBLY.png", link: "https://www.assemblyai.com/" }
+    ],
+    animesv: [
+        { nome: "Lexica", descricao: "Lexica", imagem: "img/Lexica.png", link: "https://lexica.art/" }
+    ],
+    dbvideos: [
+        { nome: "Captions", descricao: "Captions", imagem: "img/Captions.png", link: "https://www.captions.ai/?campaignid=21065325519&adgroupid=160577226218&keyword=captions&device=c&gad_source=1&gclid=CjwKCAjwl6-3BhBWEiwApN6_ktooGg-lb751lr2qA-4zVaryFlHUsRNhakoBE_lRwj0QPQfZ5EQ_MBoCAvIQAvD_BwE" },
+        { nome: "Domo AI", descricao: "Domo AI", imagem: "img/Domo.png", link: "https://domoai.app/" }
+    ],
+    finstagram: [
+        { nome: "VÍDEO FK", descricao: "VÍDEO FK", imagem: "img/FK.png", link: "https://www.videofk.com/pt/ixigua-video-download" },
+        { nome: "Save insta", descricao: "Save insta", imagem: "img/Save.png", link: "https://saveclip.app/pt" },
+        { nome: "IG Downloader", descricao: "IG Downloader", imagem: "img/IG.png", link: "https://clipdown.app/pt" },
+        { nome: "FAST DL", descricao: "FAST DL", imagem: "img/FAST.png", link: "https://fastdl.app/pt" },
+        { nome: "IGRAM", descricao: "IGRAM", imagem: "img/IGRAM.png", link: "https://igram.world/pt/" }
+    ],
+    ftik: [
+        { nome: "ssstik", descricao: "ssstik", imagem: "img/ssstik.png", link: "https://ssstik.io/pt" },
+        { nome: "SNAPTIK", descricao: "SNAPTIK", imagem: "img/SNAPTIK.png", link: "https://snaptik.app/pt2#google_vignette" },
+        { nome: "SAVETT", descricao: "SAVETT", imagem: "img/SAVETT.png", link: "https://savett.cc/en1/" },
+        { nome: "Savetik", descricao: "Savetik", imagem: "img/Savetik.png", link: "https://savetik.co/pt1" },
+        { nome: "VÍDEO FK", descricao: "VÍDEO FK", imagem: "img/FK.png", link: "https://www.videofk.com/pt/ixigua-video-download" }
+    ],
+    etiktok: [
+        { nome: "Sort tiktok", descricao: "Sort tiktok OBS: ISSO É UMA EXTENSÃO PARA USAR NO NAVEGADOR DENTRO DO TIKTOK, ELA SEPARA OS POST COM MAIS VIEWS, CURTIDAS OU COMENTÁRIOS PARA VOCÊ DE MANEIRA FÁCIL E PRÁTICA.", imagem: "img/Sort.png", link: "https://chromewebstore.google.com/detail/sort-for-tiktok-videos-by/dpfmkbcoaddghkkhebjcjaoaiaefigln" }
+    ],
+    dyoutube: [
+        { nome: "Speak", descricao: "Speak", imagem: "img/Speak.png", link: "https://chromewebstore.google.com/detail/speak-subtitles-for-youtu/fjoiihoancoimepbgfcmopaciegpigpa" }
+    ],
+    rimagem: [
+        { nome: "Remove BG", descricao: "Remove BG", imagem: "img/BG.png", link: "https://www.remove.bg/pt-br" }
+    ],
+    enavegador: [
+        { nome: "DARK READER", descricao: "DARK READER", imagem: "img/READER.png", link: "https://chromewebstore.google.com/detail/dark-reader/eimadpbcbfnmbkopoojfekhnkhdbieeh?hl=pt-BR" }
+    ]
+};
+
+function mostrarFerramentas(categoria) {
+    const lista = document.getElementById("listaFerramentas");
+    lista.innerHTML = "";
+    ferramentas[categoria].forEach(ferramenta => {
+        const item = document.createElement("li");
+        item.innerHTML = `<img src="${ferramenta.imagem}" alt="${ferramenta.nome}" class="icon"> ${ferramenta.nome}`;
+        item.onclick = () => mostrarDetalhes(ferramenta);
+        lista.appendChild(item);
+    });
+    document.getElementById("ferramentas").classList.remove("hidden");
+    document.getElementById("detalhes").classList.add("hidden");
+}
+
+function mostrarDetalhes(ferramenta) {
+    document.getElementById("imagemFerramenta").src = ferramenta.imagem;
+    document.getElementById("descricaoFerramenta").textContent = ferramenta.descricao;
+    document.getElementById("linkFerramenta").href = ferramenta.link;
+    document.getElementById("linkFerramenta").textContent = "Acesse aqui";
+    document.getElementById("detalhes").classList.remove("hidden");
+}
+
+//VISITANTES
+    // Obtém o valor atual do contador no localStorage
+    let visitas = localStorage.getItem("contadorVisitantes");
+
+    // Se não existir, começa do zero
+    if (!visitas) {
+        visitas = 0;
+    } else {
+        visitas = parseInt(visitas);
+    }
+
+    // Aumenta o número de visitas
+    visitas++;
+
+    // Salva o novo valor no localStorage
+    localStorage.setItem("contadorVisitantes", visitas);
+
+    // Exibe no HTML
+    document.getElementById("visitantes").innerText = "" + visitas;
